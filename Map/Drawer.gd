86e931ee71a30e6c1154928b2c1962619513bd23
Vladislav Smirnov <beluckydaf@gmail.com>
2020-09-29 19:40:37 +0300

    Remove adding blocks, redundant.

--- Map/Drawer.gd
+++ Map/Drawer.gd
@@ -8,6 +8,8 @@ var limit = 2
 
 var tree : QuadTree
 
+var debug_draw_tree : bool = false
+
 onready var block := preload("res://Blocks/Block.tscn")
 
 signal on_map_updated(new_map)
@@ -45,8 +47,6 @@ func set_map(value, multiplier):
 func _on_Drawer_on_map_updated(new_map):
 	generate_tree()
 	update()
-	#_remove_blocks()
-	#_put_blocks()
 	emit_signal("on_map_drawn", mul)
 
 func _draw():
@@ -62,34 +62,17 @@ func _draw():
 				stack.push_back(node.child_se)
 			elif node.state == 1:
 				draw_rect(Rect2(node.bounds.position * mul, node.bounds.size * mul), Color.black)
-			#draw_rect(Rect2(node.bounds.position * mul, node.bounds.size * mul), Color.crimson, false, 2)
-
-func _remove_blocks():
-	for child in get_children():
-		child.queue_free()
-
-func _put_blocks():
-	var map_node = self # get_tree().root.get_node_or_null("Map")
-	if map_node == null:
-		map_node = Node2D.new()
-		map_node.name = "Map"
-		get_tree().root.call_deferred("add_child", map_node)
-	else:
-		for child in map_node.get_children():
-			child.call_deferred("queue_free")
-	for y in range(map.height):
-		for x in range(map.width):
-			if map.plain[y * map.width + x] == 1:
-				var instance = block.instance()
-				instance.size = Vector2(mul, mul)
-				instance.position = Vector2(mul/2, mul/2) + Vector2(x, y)*mul
-				map_node.add_child(instance)
+			if debug_draw_tree:
+				draw_rect(Rect2(node.bounds.position * mul, node.bounds.size * mul), Color.crimson, false, 2)
 
 func _input(event):
-	if event.is_action_pressed("ui_accept"):
+	if event.is_action("ui_accept"):
 		var x = round(rand_range(0, 255))
 		var y = round(rand_range(0, 255))
-		tree.insert_rect(Rect2(Vector2(x, y), Vector2.ONE*10), 1)
+		tree.insert_rect(Rect2(Vector2(x, y), Vector2.ONE*15), 0)
+		update()
+	elif event.is_action_pressed("ui_select"):
+		debug_draw_tree = not debug_draw_tree
 		update()
 
 func _on_Drawer_on_map_drawn(multiplier):
